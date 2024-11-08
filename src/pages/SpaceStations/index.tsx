bbf6b2dc602109f087b23cb951d9fb736fbdadad
Jonatas Freire <jonatasf1997@hotmail.com>
2024-11-08 00:29:14 -0300

    it renders

--- src/pages/SpaceStations/index.tsx
+++ src/pages/SpaceStations/index.tsx
@@ -5,14 +5,42 @@ import { useGetSpaceStations } from "@/services";
 import { useFilterResponse } from "@/hooks/useFilterResponse/useFilterResponse";
 import { useSpaceStationsFilter } from "./hooks/useSpaceStationsFilter/useSpaceStationsFilter";
 import { FilterLayout } from "@/components/templates/FilterLayout/FilterLayout";
-import { Teste } from "./teste";
 
 export const SpaceStations = () => {
-  const { definitions } = useSpaceStationsFilter();
+  const { data, refetch, isLoading } = useGetSpaceStations();
+  const { definitions, spaceStationFilterData } = useSpaceStationsFilter();
+
+  useEffect(() => {
+    refetch();
+  }, [spaceStationFilterData, refetch]);
+
+  const filterFunc = useCallback(
+    (serrch: string) =>
+      data?.results.filter(
+        (station) =>
+          station.name.toLowerCase().includes(serrch) ||
+          station.founded.toLowerCase().includes(serrch)
+      ),
+    [data?.results]
+  );
+
+  const { mapedData } = useFilterResponse<SpaceStation>(
+    data?.results,
+    filterFunc
+  );
+
+  const render = () => {
+    if (isLoading) return <div>loagind..</div>;
+    if (!mapedData) return <div>not Found</div>;
+    if (mapedData)
+      return mapedData.map((spaceStation) => (
+        <SpaceStationsCard spaceStation={spaceStation} key={spaceStation.id} />
+      ));
+  };
 
   return (
     <FilterLayout props={{ definitions, title: "Space Stations" }}>
-      <Teste />
+      {render()}
     </FilterLayout>
   );
 };
